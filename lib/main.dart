116f1fb19da7038febd84ee0b312750864c27bd3
isaque <isaquesantosdequeirozq@gmail.com>
2025-06-25 13:39:38 -0300

    definindo as rotas do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import "home_page.dart";
-import "add_pet.dart";
-import "pet_details.dart";
-import "pet_vaccines.dart";
-import "add_vaccine.dart";
-import "pet_events.dart";
-import "add_pet_event.dart";
+import "pages/home_page.dart";
+import "pages/add_pet.dart";
+import "pages/pet_details.dart";
+import "pages/pet_vaccines.dart";
+import "pages/add_pet_vaccine.dart";
+import "pages/pet_events.dart";
+import "pages/add_pet_event.dart";
+import "my_app_routes.dart";
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +29,13 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: "/petDetails",
       routes: {
-        "/": (BuildContext context)=> HomePage(),
-        "/addPet": (BuildContext context)=> AddPet(),
-        "/petDetails":(BuildContext context)=> PetDetails(),
-        "/petVaccines":(BuildContext context)=> PetVaccines(),
-        "/addVaccine":(BuildContext context)=> AddVaccine(),
-        "/petEvents":(BuildContext context)=> PetEvents(),
-        "/addPetEvents":(BuildContext context)=> AddPetEvent(),
+        MyAppRoutes.homePage.name: (BuildContext context)=> HomePage(),
+        MyAppRoutes.addPet.name: (BuildContext context)=> AddPet(),
+        MyAppRoutes.petDetails.name:(BuildContext context)=> PetDetails(),
+        MyAppRoutes.petVaccines.name:(BuildContext context)=> PetVaccines(),
+        MyAppRoutes.addPetVaccine.name:(BuildContext context)=> AddPetVaccine(),
+        MyAppRoutes.petEvents.name:(BuildContext context)=> PetEvents(),
+        MyAppRoutes.addPetEvent.name:(BuildContext context)=> AddPetEvent(),
       },
     );
   }
