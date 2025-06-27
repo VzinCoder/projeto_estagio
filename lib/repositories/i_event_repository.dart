0edbab17f72569049152b3557ad2b0cc4fb21ef8
Roberth da Silva Mendes <roberthda2332@gmail.com>
2025-06-27 14:01:24 -0300

    Implementando o repositório de evento.

---
+++ lib/repositories/i_event_repository.dart
@@ -0,0 +1,9 @@
+import 'package:projeto_estagio/models/event.dart';
+
+abstract class IEventRepository {
+  Future<int> insertEvent(Event event);
+  Future<List<Event>> getAllEvents();
+  Future<Event?> getEventById(int id);
+  Future<int> updateEvent(Event event);
+  Future<int> deleteEvent(int id);
+}
