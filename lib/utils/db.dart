2f503774ffa49c0089414b533704b02454fe089e
isaque <isaquesantosdequeirozq@gmail.com>
2025-07-06 16:20:42 -0300

    tabelas reestruturadas para os campos id do tipo text

--- lib/utils/db.dart
+++ lib/utils/db.dart
@@ -51,7 +51,7 @@ class Db {
   Future<void> _onCreate(Database db, int version) async {
     await db.execute('''
       CREATE TABLE animals (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id TEXT PRIMARY KEY,
         name TEXT NOT NULL,
         type TEXT NOT NULL,
         breed TEXT NOT NULL,
@@ -61,8 +61,8 @@ class Db {
 
     await db.execute('''
       CREATE TABLE vaccines (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        animal_id INTEGER NOT NULL,
+        id TEXT PRIMARY KEY,
+        animal_id TEXT NOT NULL,
         name TEXT NOT NULL,
         application_date TEXT NOT NULL,
         next_dose_date TEXT,
@@ -72,8 +72,8 @@ class Db {
 
     await db.execute('''
       CREATE TABLE events (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        animal_id INTEGER NOT NULL,
+        id TEXT PRIMARY KEY,
+        animal_id TEXT NOT NULL,
         type TEXT NOT NULL,
         date TEXT NOT NULL,
         observation TEXT,
