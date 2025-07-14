8ef23402c21fbdee3f7aac2e42fa4ec530a16ac2
isaque <isaquesantosdequeirozq@gmail.com>
2025-07-14 14:09:02 -0300

    implementando a checagem de valores para upload e o upload

--- lib/pages/sync_page.dart
+++ lib/pages/sync_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_estagio/pages/home_page.dart';
 import 'package:projeto_estagio/repositories/api_repository.dart';
+import 'package:projeto_estagio/repositories/pet_repository.dart';
 import '../utils/injector.dart';
 
 class SyncPage extends StatefulWidget {
@@ -58,8 +59,13 @@ class _SyncPageState extends State<SyncPage> {
   }
 
   Future<Map<String, int>> _fetchPendingUploads() async {
-    await Future.delayed(const Duration(milliseconds: 300));
-    return {'animals': 3, 'events': 5, 'vaccines': 2};
+    final response = await apiRepository.checkUploadUpdates();
+
+    if(response['has_updates']){
+      return Map.castFrom<String, dynamic, String, int>(response['update_counts']); 
+    }
+
+    return {};
   }
 
   String _formatDownloadInfo(Map<String, int> data) {
@@ -120,7 +126,7 @@ class _SyncPageState extends State<SyncPage> {
   }
 
   Future<void> _performUpload() async {
-    await Future.delayed(const Duration(seconds: 2));
+    await apiRepository.uploadData();
   }
 
   void _showLoadingDialog(String message) {
